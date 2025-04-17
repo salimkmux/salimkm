f5eca4419a4cd5f1fd503c80ce58433c011747b7
DESKTOP-RD903AQ\MUHAMMAD SALIM <salimkmux@gmail.com>
2025-04-17 16:13:17 +0400

    first commit

---
+++ src/components/hero/Speech.jsx
@@ -0,0 +1,32 @@
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "motion/react";
+
+const Speech = () => {
+  return (
+    <motion.div
+      className="bubbleContainer"
+      animate={{ opacity: [0, 1] }}
+      transition={{ duration: 1 }}
+    >
+      <div className="bubble">
+        <TypeAnimation
+          sequence={[
+            1000,
+            "Looking for a long association with people-oriented organization",
+            1000,
+            " I can use my managerial skills and abilities that offer professional growth.",
+            1000,
+          ]}
+          wrapper="span"
+          speed={40}
+          deletionSpeed={60}
+          // omitDeletionAnimation
+          repeat={Infinity}
+        />
+      </div>
+      <img src="/hero2.png" alt="" />
+    </motion.div>
+  );
+};
+
+export default Speech;
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,190 @@
+import { useEffect, useRef, useState } from "react";
+import "./portfolio.css";
+import { motion, useInView, useScroll, useTransform } from "motion/react";
+
+const items = [
+  {
+    id: 1,
+    img: "/p1.png",
+    title: "Multi-Vendor Ecommerce Marketplace",
+    desc: "Chaamber is a dynamic and user-friendly online marketplace where a diverse community of independent sellers can showcase and sell their unique products to a wide range of customers. We provide a platform that empowers entrepreneurs, artisans, and businesses of all sizes to reach a broader audience and thrive in the digital landscape.",
+    link: "https://chambar-v2.vercel.app/",
+  },
+  {
+    id: 2,
+    img: "/p2.png",
+    title: "Admin Management Dashboard",
+    desc: "In a single-user multiple company admin dashboard scenario the virtual control room enables one administrator to manage the diverse operations of several distinct organizations or business units simultaneously on a single platform.",
+    link: "https://react.quicks.ae/",
+    target: "_blank"
+  },
+  {
+    id: 3,
+    img: "/p3.png",
+    title: "Sa’aeh Medical Tourism",
+    desc: "Sa’aeh will be the major platform for so many aspects and economical sectors , the country would like to take care of and ....",
+    link: "/",
+  },
+  {
+    id: 4,
+    img: "/p4.png",
+    title: "TA`LEEM Education Project",
+    desc: "Taleem is a comprehensive online education center and digital library designed to empower learners of all ages and backgrounds. ",
+    link: "/",
+  },
+  {
+    id: 5,
+    img: "/p5.png",
+    title: "Hidoctor Healthcare facilitating easy",
+    desc: "Hidoctor is UAE's largest patient centric, healthcare discovery and engagement platform, facilitating easy and quick access to healthcare.",
+    link: "/",
+  },
+];
+
+const imgVariants = {
+  initial: {
+    x: -500,
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const textVariants = {
+  initial: {
+    x: 500,
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+      staggerChildren: 0.05,
+    },
+  },
+};
+
+const ListItem = ({ item }) => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
+  return (
+    <div className="pItem" ref={ref}>
+      <motion.div
+        variants={imgVariants}
+        animate={isInView ? "animate" : "initial"}
+        className="pImg"
+      >
+        <img src={item.img} alt="" />
+      </motion.div>
+      <motion.div
+        variants={textVariants}
+        animate={isInView ? "animate" : "initial"}
+        className="pText"
+      >
+        <motion.h1 variants={textVariants}>{item.title}</motion.h1>
+        <motion.p variants={textVariants}>{item.desc}</motion.p>
+        <motion.a variants={textVariants} href={item.link}>
+          <button>View Project</button>
+        </motion.a>
+      </motion.div>
+    </div>
+  );
+};
+
+const Portfolio = () => {
+  const [containerDistance, setContainerDistance] = useState(0);
+  const ref = useRef(null);
+
+  // useEffect(() => {
+  //   if (ref.current) {
+  //     const rect = ref.current.getBoundingClientRect();
+  //     setContainerDistance(rect.left);
+  //   }
+  // }, []);
+
+  // FIX: Re-calculate when screen size changes
+  useEffect(() => {
+    const calculateDistance = () => {
+      if (ref.current) {
+        const rect = ref.current.getBoundingClientRect();
+        setContainerDistance(rect.left);
+      }
+    };
+
+    calculateDistance();
+
+    window.addEventListener("resize", calculateDistance);
+
+    return () => {
+      window.removeEventListener("resize", calculateDistance);
+    };
+  }, []);
+
+  const { scrollYProgress } = useScroll({ target: ref });
+
+  const xTranslate = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, -window.innerWidth * items.length]
+  );
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <motion.div className="pList" style={{ x: xTranslate }}>
+        <div
+          className="empty"
+          style={{
+            width: window.innerWidth - containerDistance,
+            // backgroundColor: "pink",
+          }}
+        />
+        {items.map((item) => (
+          <ListItem item={item} key={item.id} />
+        ))}
+      </motion.div>
+      <section />
+      <section />
+      <section />
+      <section />
+      <section />
+      <div className="pProgress">
+        <svg width="100%" height="100%" viewBox="0 0 160 160">
+          <circle
+            cx="80"
+            cy="80"
+            r="70"
+            fill="none"
+            stroke="#ddd"
+            strokeWidth={20}
+          />
+          <motion.circle
+            cx="80"
+            cy="80"
+            r="70"
+            fill="none"
+            stroke="#dd4c62"
+            strokeWidth={20}
+            style={{ pathLength: scrollYProgress }}
+            transform="rotate(-90 80 80)"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
